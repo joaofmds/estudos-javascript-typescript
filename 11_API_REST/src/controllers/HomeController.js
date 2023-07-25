390a4c953cb1ccfe7c0c6af2e4d4a90a7f64b247
joaofmds <ms.joao.felipe@gmail.com>
2023-07-25 10:01:15 -0300

    Realiza CRUD de usuários no banco de dados

--- 11_API_REST/src/controllers/HomeController.js
+++ 11_API_REST/src/controllers/HomeController.js
@@ -1,5 +1,15 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Luiz',
+      sobrenome: 'Santos',
+      email: 'luiz@gmail.com',
+      idade: 112,
+      peso: 300,
+      altura: 2.5,
+    });
     res.json({
       tudoCerto: true,
     });
