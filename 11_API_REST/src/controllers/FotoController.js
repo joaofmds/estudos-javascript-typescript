b87f013649aee2b2c4aa66648b6ee70697ede198
joaofmds <ms.joao.felipe@gmail.com>
2023-07-25 17:18:05 -0300

    Trata os arquivos enviados

--- 11_API_REST/src/controllers/FotoController.js
+++ 11_API_REST/src/controllers/FotoController.js
@@ -1,6 +1,19 @@
+import multer from 'multer';
+import multerConfig from '../database/multerConfig';
+
+const upload = multer(multerConfig).single('foto');
+
 class FotoController {
   async index(req, res) {
-    res.json(req.file);
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(401).json({
+          errors: [err.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
--- 11_API_REST/src/routes/fotoRoutes.js
+++ 11_API_REST/src/routes/fotoRoutes.js
@@ -1,12 +1,8 @@
 import { Router } from 'express';
-import multer from 'multer';
 import fotoController from '../controllers/FotoController';
-import multerConfig from '../database/multerConfig';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('foto'), fotoController.index);
+router.post('/', fotoController.index);
 
 export default router;
