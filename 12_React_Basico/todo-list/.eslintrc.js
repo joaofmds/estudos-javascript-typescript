43f6553a3d70cd63f0726f28ccced1e6893df7ff
João Souza <ms.joao.felipe@gmail.com>
2023-08-08 22:34:32 -0300

    React com componentes de classe (Básico) - JavaScript

---
+++ 12_React_Basico/todo-list/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parser: 'babel-eslint',
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    "react/jsx-filename-extension": 0,
+    "react/state-in-constructor": 0,
+    "react/forbid-prop-types": 0,
+  },
+};
